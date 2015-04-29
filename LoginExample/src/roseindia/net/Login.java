3a78c16f9802453e7e093eab601d683877a9d672
lushangshu <slu7@sheffield.ac.uk>
2015-04-29 17:42:58 +0100

    template
    1

--- LoginExample/src/roseindia/net/Login.java
+++ LoginExample/src/roseindia/net/Login.java
@@ -2,6 +2,10 @@ package roseindia.net;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
@@ -9,6 +13,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.mysql.jdbc.Statement;
 
 /**
  * Servlet implementation class Login
@@ -31,6 +37,58 @@ public class Login extends HttpServlet {
 	public void init(ServletConfig config) throws ServletException {
 		// TODO Auto-generated method stub
 	}
+	
+	public String connectWithSQL(String un,String pw)
+	{
+		try {
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+			System.out.println("Driver found");
+		} catch (ClassNotFoundException e) {
+			System.out.println("Driver not found" + e);
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+//		String url = "jdbc:mysql://stusql.dcs.shef.ac.uk";
+//		String user = "team153";
+//		String password = "80473623";
+		String DB = "jdbc:mysql://stusql.dcs.shef.ac.uk/team153?user=team153&password=80473623";
+	
+		Connection con = null;
+		try {
+			con = DriverManager.getConnection(DB);
+			Statement myStmt = (Statement) con.createStatement();
+			ResultSet myRs = myStmt.executeQuery("select * from test");
+			System.out.println("Connected");
+			while(myRs.next())
+			{
+				//System.out.println(myRs.getString("username"));
+//				if(un.equals(myRs.getString("username"))&&pw.equals(myRs.getString("password")))
+//				{
+//					return un;
+//				}
+				System.out.println(myRs.getString("username"));
+				if(un.equals(myRs.getString("username")))
+				{
+					return un;
+				}
+				
+			}
+			
+			return myRs.getString("username")+"not found";
+			
+		} catch (SQLException e) {
+			System.out.println("Connection failed");
+			String errorLog = "Failed";
+			return errorLog;
+		}
+		//return true;
+		
+	}
 
 	/**
 	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
@@ -45,18 +103,14 @@ public class Login extends HttpServlet {
 		String pw = request.getParameter("password");
 		
 		String msg = "";
-		if(un.equals(username)&&pw.equals(password))
-		{
-			msg = un + "login successful";
-		}
-		else
-		{
-			msg = "error";
-		}
+		String feedback = connectWithSQL(un,pw);
+		//String feedback = "Login successed";
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
+		//out.print(feedback);
+		//response.sendRedirect("http://localhost:8080/LoginExample/signup.jsp");
 		
-		out.println("<font size = '6' color = red>"+msg +"</font>");
+		out.println("<font size = '6' color = red>"+feedback +"</font>");
 		
 	}
 
